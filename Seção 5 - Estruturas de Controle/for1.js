b58544c4327e1502d7fba14f9e21aab11789d08a
Igor Nunes <igoralunomc@gmail.com>
2022-03-28 13:11:31 -0300

    Curso JS - Udemy

---
+++ Seção 5 - Estruturas de Controle/for1.js
@@ -0,0 +1,18 @@
+let contador = 1
+while(contador <= 10) {
+    console.log(`Contador = ${contador}`)
+    contador++
+}
+
+console.log('-------------')
+
+for(let i = 1; i <= 10; i++) {
+    console.log(`Contador = ${i}`)
+}
+
+console.log('-------------')
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+for(i = 0; i < notas.length; i++) {
+    console.log(`Nota = ${notas[i]}`)
+}
