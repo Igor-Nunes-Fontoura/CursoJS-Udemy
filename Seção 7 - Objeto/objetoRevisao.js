2dbe9dc8f21acb25a013e183dc5a8b988ce9596a
Igor Nunes <igoralunomc@gmail.com>
2022-04-02 17:59:44 -0300

    Curso JS - Udemy

---
+++ Seção 7 - Objeto/objetoRevisao.js
@@ -0,0 +1,32 @@
+// Coleção dinâmica de pares chave/valor
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['marca do produto'] = 'Generica'
+produto.preco = 220
+
+console.log(produto)
+
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    proprietario: {
+        nome: 'Raul',
+        idade: 56,
+        endereco: {
+            logradouro: 'Rua ABC',
+            numero: 123
+        }
+    },
+    condutores: [{
+        nome: 'Junior',
+        idade: 19
+    }, {
+        nome: 'Ana',
+        idade: 42
+    }],
+    cacularValorSeguro: function() {
+        // ...
+    }
+}
+
+console.log(carro)
